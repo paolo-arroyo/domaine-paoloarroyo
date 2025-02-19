6c723d6e6a4a52bcba3c278076e3baa64c09b9ce
Paolo Arroyo <juanpaoloarroyo@gmail.com>
2025-02-20 03:17:48 +0800

    config: project configuration

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+
+let plugin = require('tailwindcss/plugin');
+
+module.exports = {
+  content: ['./**/*.liquid', './frontend/**/*.{js,ts,jsx,tsx}'],
+  safelist: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
